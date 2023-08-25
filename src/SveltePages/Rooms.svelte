bc2961630160b05200db90923bac963800f3fd78
Alireza Balouch <gswape@gmail.com>
2023-08-25 10:54:18 +0200

    Rooms

---
+++ src/SveltePages/Rooms.svelte
@@ -0,0 +1,15 @@
+<script>
+import {rooms} from '../store.js'
+import Card from '../components/Card.svelte'
+</script>
+
+<section class="grid grid-cols-1 sm:grid-cols-2 gap-3 m-4 xl:grid-cols-3">
+  {#each $rooms as room}
+    <Card title={room.title}>
+      <p>{room.description}</p>
+      <div class="card-actions">
+        <button class="btn btn-secondary">knappen</button>
+      </div>
+    </Card>
+  {/each}
+</section>
