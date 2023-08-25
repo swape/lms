560e53ed6951833c52c1c4ae6073298f7584fcba
Alireza Balouch <gswape@gmail.com>
2023-08-25 10:55:39 +0200

    authGate

---
+++ src/components/AuthGate.svelte
@@ -0,0 +1,16 @@
+<script>
+import Login from './Login.svelte'
+import {auth, roles, user} from '../store.js'
+import RoleSelect from './RoleSelect.svelte'
+export let showLogin = false
+</script>
+
+{#if $auth}
+  {#if $user}
+    <slot />
+  {:else if showLogin}
+    <RoleSelect />
+  {/if}
+{:else if showLogin}
+  <Login />
+{/if}
