1fdee039e68539eafad8b688ff2f8a6dbb505309
Alireza Balouch <gswape@gmail.com>
2024-01-18 15:31:58 +0100

    correct import

--- src/components/Login.svelte
+++ src/components/Login.svelte
@@ -1,5 +1,6 @@
 <script>
 import {auth} from '../store.js'
+import '@passageidentity/passage-elements/passage-auth'
 
 const PUBLIC_PASSAGE_APP_ID = import.meta.env.PUBLIC_PASSAGE_APP_ID
 function onClick() {
@@ -10,7 +11,7 @@ function onClick() {
 {#if !$auth}
 <section class="container mx-auto">
   <div class="text-3xl py-4">Login page</div>
-  <passage-auth app-id={PUBLIC_PASSAGE_APP_ID}></passage-auth>
+  {#if PUBLIC_PASSAGE_APP_ID}<passage-auth app-id={PUBLIC_PASSAGE_APP_ID}></passage-auth>{/if}
 
   <button on:click={onClick} type="button" class="btn btn-primary">Fake login</button>
 </section>
