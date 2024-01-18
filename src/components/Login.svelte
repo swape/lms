971124777527f1844f467bedc9be1d87bd7e5726
Alireza Balouch <gswape@gmail.com>
2024-01-18 15:55:07 +0100

    passage auth

--- src/components/Login.svelte
+++ src/components/Login.svelte
@@ -1,17 +1,33 @@
 <script>
 import {auth} from '../store.js'
-import '@passageidentity/passage-elements/passage-auth'
+
+import {onMount} from 'svelte'
+import { Passage } from '@passageidentity/passage-js';
 
 const PUBLIC_PASSAGE_APP_ID = import.meta.env.PUBLIC_PASSAGE_APP_ID
+const passage = new Passage(PUBLIC_PASSAGE_APP_ID)
+const user = passage.getCurrentUser()
 function onClick() {
   // check real auth
   $auth = true
 }
+
+onMount(async () => {
+  // check real auth
+  const userInfo = await user.userInfo()
+
+  if(userInfo?.id){
+    $auth = true
+  }
+})
+
 </script>
 {#if !$auth}
 <section class="container mx-auto">
   <div class="text-3xl py-4">Login page</div>
-  {#if PUBLIC_PASSAGE_APP_ID}<passage-auth app-id={PUBLIC_PASSAGE_APP_ID}></passage-auth>{/if}
+  {#if PUBLIC_PASSAGE_APP_ID}
+    <passage-auth app-id={PUBLIC_PASSAGE_APP_ID}></passage-auth>
+  {/if}
 
   <button on:click={onClick} type="button" class="btn btn-primary">Fake login</button>
 </section>
