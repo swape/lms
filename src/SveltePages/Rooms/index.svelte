8c0992e9071616cd8d1f98ed0d35af8af38cedb0
Alireza Balouch <gswape@gmail.com>
2024-03-28 15:56:21 +0100

    Add isAdmin flag and filter rooms based on user/group

--- src/SveltePages/Rooms/index.svelte
+++ src/SveltePages/Rooms/index.svelte
@@ -1,14 +1,21 @@
 <script>
-import {rooms, currentRoom, user} from '../../store.js'
+import {rooms, currentRoom, user, currentRole} from '../../store.js'
 import Card from '../../components/Card.svelte'
 import Room from './Room.svelte'
-import {testRooms} from './testRooms.ts'
 
-user.subscribe((value) => {
-  if (value) {
-    rooms.set(testRooms)
+let isAdmin = false
+let userRooms = []
+
+currentRole.subscribe((value) => {
+  if (value?.level === 4) {
+    isAdmin = true
+    userRooms = $rooms
+  }
+  else{
+    // TODO filter rooms based on user/group
   }
 })
+
 function selectRoom(room) {
   $currentRoom = room
 }
@@ -17,8 +24,17 @@ function selectRoom(room) {
 {#if $currentRoom}
   <Room />
 {:else}
+<div class="flex justify-between p-4">
+  <h1 class="text-3xl text-center grow">Klasseromene</h1>
+  {#if isAdmin}
+  <div class="">
+    <button class="btn btn-circle btn-primary btn-sm" >+</button>
+  </div>
+      {/if}
+</div>
+
   <section class="grid grid-cols-1 sm:grid-cols-2 gap-3 m-4 xl:grid-cols-3">
-    {#each $rooms as room}
+    {#each userRooms as room}
       <Card title={room.title}>
         <p>{room.description}</p>
         <div class="card-actions">
