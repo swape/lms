9fd5d23b67c07e004cc30ae2cb0ea1f3a5c1d937
Alireza Balouch <gswape@gmail.com>
2024-03-29 16:25:47 +0100

    Add updateRoom and addRoom functions to rooms.js

--- src/apiCalls/rooms.js
+++ src/apiCalls/rooms.js
@@ -3,3 +3,11 @@ import {supabaseClient} from '../supabaseConfig'
 export function getRooms(sid) {
   return supabaseClient.from('rooms').select('*').eq('sid', sid)
 }
+
+export function updateRoom(room) {
+  return supabaseClient.from('rooms').update(room).eq('id', room.id)
+}
+
+export function addRoom(room) {
+  return supabaseClient.from('rooms').insert(room)
+}
