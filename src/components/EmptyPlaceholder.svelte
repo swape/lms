68dad01d723e1645fb076016f58354d788ddef20
Alireza Balouch <gswape@gmail.com>
2024-03-29 16:25:35 +0100

    Add EmptyPlaceholder component with SVG illustration

---
+++ src/components/EmptyPlaceholder.svelte
@@ -0,0 +1,8 @@
+<script>
+export let message = 'Ingen data'
+</script>
+
+<div class="p-4 flex flex-col gap-4 items-center">
+  <img src="/svg/undraw_void_-3-ggu.svg" alt="Illustrasjon av et tomt rom" class="max-w-sm w-full" />
+  <p class="pt-4">{message}</p>
+</div>
