2be59bcc6907945c54e4c483d0ce057f65723b99
Alireza Balouch <gswape@gmail.com>
2024-03-29 14:53:01 +0100

    Add RoomList component and refactor Room index page

---
+++ src/SveltePages/Rooms/RoomList.svelte
@@ -0,0 +1,20 @@
+<script>
+import {currentRoom} from '../../store.js'
+import Card from '../../components/Card.svelte'
+export let userRooms = []
+
+function selectRoom(room) {
+  $currentRoom = room
+}
+</script>
+
+<section class="grid grid-cols-1 sm:grid-cols-2 gap-3 m-4 xl:grid-cols-3">
+  {#each userRooms as room}
+    <Card title={room.title}>
+      <p>{room.description}</p>
+      <div class="card-actions">
+        <button class="btn btn-primary btn-sm" on:click={() => selectRoom(room)} type="button">Vis</button>
+      </div>
+    </Card>
+  {/each}
+</section>
--- src/SveltePages/Rooms/index.svelte
+++ src/SveltePages/Rooms/index.svelte
@@ -1,7 +1,8 @@
 <script>
-import {rooms, currentRoom, user, currentRole} from '../../store.js'
-import Card from '../../components/Card.svelte'
+import {rooms, currentRoom, currentRole} from '../../store.js'
+
 import Room from './Room.svelte'
+import RoomList from './RoomList.svelte'
 
 let isAdmin = false
 let userRooms = []
@@ -14,10 +15,6 @@ currentRole.subscribe((value) => {
     // TODO filter rooms based on user/group
   }
 })
-
-function selectRoom(room) {
-  $currentRoom = room
-}
 </script>
 
 {#if $currentRoom}
@@ -32,14 +29,5 @@ function selectRoom(room) {
     {/if}
   </div>
 
-  <section class="grid grid-cols-1 sm:grid-cols-2 gap-3 m-4 xl:grid-cols-3">
-    {#each userRooms as room}
-      <Card title={room.title}>
-        <p>{room.description}</p>
-        <div class="card-actions">
-          <button class="btn btn-primary btn-sm" on:click={() => selectRoom(room)} type="button">knappen</button>
-        </div>
-      </Card>
-    {/each}
-  </section>
+  <RoomList userRooms={userRooms} />
 {/if}
