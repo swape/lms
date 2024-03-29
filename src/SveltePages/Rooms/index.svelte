26a1380527d1f11133aacce6356fc9d51ab5ccc5
Alireza Balouch <gswape@gmail.com>
2024-03-29 16:26:24 +0100

    Add EditRoom component and Modal for adding new rooms

--- src/SveltePages/Rooms/index.svelte
+++ src/SveltePages/Rooms/index.svelte
@@ -1,16 +1,21 @@
 <script>
 import {rooms, currentRoom, currentRole} from '../../store.js'
+import EditRoom from './EditRoom.svelte'
+import Modal from '../../components/Modal.svelte'
+import EmptyPlaceholder from '../../components/EmptyPlaceholder.svelte'
 
 import Room from './Room.svelte'
 import RoomList from './RoomList.svelte'
 
 let isAdmin = false
 let userRooms = []
+let sid = 0
 
 currentRole.subscribe((value) => {
   if (value?.level === 4) {
     isAdmin = true
     userRooms = $rooms
+    sid = value.sid
   } else {
     // TODO filter rooms based on user/group
   }
@@ -23,11 +28,14 @@ currentRole.subscribe((value) => {
   <div class="flex justify-between p-4">
     <h1 class="text-3xl text-center grow">Klasseromene</h1>
     {#if isAdmin}
-      <div class="">
-        <button class="btn btn-circle btn-primary btn-sm">+</button>
-      </div>
+      <Modal id="add-user" btnClass="btn btn-circle btn-primary btn-sm material-symbols-outlined" openText="add_circle">
+        <EditRoom sid={sid} />
+      </Modal>
     {/if}
   </div>
 
   <RoomList userRooms={userRooms} />
+  {#if userRooms.length === 0 && isAdmin}
+    <EmptyPlaceholder message="Her kan du legge til nye klasserom" />
+  {/if}
 {/if}
