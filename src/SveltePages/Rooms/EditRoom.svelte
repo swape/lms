3e948513df54e7f5c4a217c7b113304305469961
Alireza Balouch <gswape@gmail.com>
2024-03-30 11:37:33 +0100

    handling errors

--- src/SveltePages/Rooms/EditRoom.svelte
+++ src/SveltePages/Rooms/EditRoom.svelte
@@ -3,6 +3,7 @@ import {updateRoom, addRoom} from '../../apiCalls/rooms.js'
 import {createEventDispatcher} from 'svelte'
 import {populateRoomsAndGroups} from '../../services.js'
 import {currentRoom} from '../../store.js'
+import ErrorBox from '../../components/ErrorBox.svelte'
 
 const dispatch = createEventDispatcher()
 
@@ -12,30 +13,35 @@ export let defaultRoom = {
 }
 
 export let sid = 0
+let errorMessage = ''
 
 function editRoom() {
+  errorMessage = ''
   if (defaultRoom.title === '' || defaultRoom.description === '') {
-    // TODO: show error message
+    errorMessage = 'Fyll ut alle feltene'
     return
   }
   if (defaultRoom?.id) {
     updateRoom(defaultRoom).then((res) => {
-      console.log(res)
-      // TODO show error message
-      populateRoomsAndGroups(sid, true)
-      dispatch('toggle')
-      $currentRoom = defaultRoom
+      if (res.error) {
+        errorMessage = res.error?.message || 'Noe gikk galt, prøv igjen senere'
+      } else {
+        populateRoomsAndGroups(sid, true)
+        dispatch('toggle')
+        $currentRoom = defaultRoom
+      }
     })
   } else {
     defaultRoom.sid = sid
     addRoom(defaultRoom).then((res) => {
-      console.log(res)
-      // TODO show error message
-      populateRoomsAndGroups(sid, true)
-      dispatch('toggle')
+      if (res.error) {
+        errorMessage = res.error?.message || 'Noe gikk galt, prøv igjen senere'
+      } else {
+        populateRoomsAndGroups(sid, true)
+        dispatch('toggle')
+      }
     })
   }
-  console.log(defaultRoom)
 }
 </script>
 
@@ -53,6 +59,12 @@ function editRoom() {
       name="description"
       class="textarea h-24 textarea-bordered textarea-primary w-full"
       bind:value={defaultRoom.description}></textarea>
+
+    {#if errorMessage}
+      <div class="pt-4">
+        <ErrorBox message={errorMessage} />
+      </div>
+    {/if}
 
     <div class="mt-3">
       <button class="btn btn-primary btn-sm" on:click={editRoom}>Lagre</button>
