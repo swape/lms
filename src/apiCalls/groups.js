14425a9b2dfd837948d04b957f1e797f4a93405a
Alireza Balouch <gswape@gmail.com>
2024-03-31 14:07:17 +0200

    group update and add calls

--- src/apiCalls/groups.js
+++ src/apiCalls/groups.js
@@ -3,3 +3,11 @@ import {supabaseClient} from '../supabaseConfig'
 export function getGroups(sid) {
   return supabaseClient.from('groups').select('*').eq('sid', sid)
 }
+
+export function updateGroup(group) {
+  return supabaseClient.from('groups').update(group).eq('id', group.id)
+}
+
+export function addGroup(group) {
+  return supabaseClient.from('groups').insert(group)
+}
