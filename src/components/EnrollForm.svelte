e8e6e2938ae0c9375e88af49207da30461e3ccaa
Alireza Balouch <gswape@gmail.com>
2024-08-09 16:02:05 +0200

    enroll with name

--- src/components/EnrollForm.svelte
+++ src/components/EnrollForm.svelte
@@ -1,24 +1,31 @@
 <script>
   import {user, schoolNames} from '../store.js'
-  import {insertEnrollment} from '../apiCalls/enroll.js'
+  import {getEnrollmentsFromEmail, insertEnrollment} from '../apiCalls/enroll.js'
 
   let localSchools = []
   let selectedSchool = ''
   let message = ''
+  let name = ''
   let feedback = ''
 
   schoolNames.subscribe((schools) => {
     localSchools = schools.filter((school) => !`${school.title}`.toLowerCase().startsWith('test'))
+    getEnrollmentsFromEmail($user.email).then(({data}) => {
+      if (data.length > 0) {
+       feedback = 'Du har allerede sendt en forespørsel.'
+      }
+    })
   })
 
   function sendForm() {
-    insertEnrollment({sid: selectedSchool, message, email: $user.email, uid: $user.uid}).then((res) => {
-      feedback = res
+    insertEnrollment({sid: selectedSchool, message, email: $user.email, uid: $user.uid, name}).then(() => {
+      feedback = 'Meldingen er sendt til skolen'
     })
   }
 
 </script>
 <div class="text-left">
+
   {#if !feedback}
     <p class="py-6">Oi, det ser ut som om {$user.email} ikke er koblet til en konto hos oss.</p>
     <p class="pb-6">Du kan be om å bli lagt til i systemet.</p>
@@ -37,6 +44,12 @@
           </select>
         {/if}
       </div>
+
+      <div  class="flex flex-col items-start mb-4">
+        <label for="my-name" class="mb-2"><span>Navn:</span></label>
+        <input class="input input-bordered w-full max-w-xs" id="my-name" bind:value={name} />
+      </div>
+
       <div  class="flex flex-col items-start">
         <label for="my-textarea" class="mb-2"><span>Melding:</span></label>
         <textarea class="textarea textarea-bordered textarea-xs w-full max-w-xs h-36" id="my-textarea" bind:value={message}></textarea>
@@ -49,5 +62,5 @@
 
 </div>
 {#if feedback}
-  <p class="mt-4">Meldingen er sendt til skolen.</p>
+  <p class="mt-4">{feedback}</p>
 {/if}
