81ec32d8791e3169d6431ba2184de8e0fad674cc
Alireza Balouch <gswape@gmail.com>
2024-08-09 16:01:48 +0200

    get enrollments based on email

--- src/apiCalls/enroll.js
+++ src/apiCalls/enroll.js
@@ -4,6 +4,10 @@ export function getEnrollments(sid) {
   return supabaseClient.from('enroll').select('*').eq('sid', sid)
 }
 
+export function getEnrollmentsFromEmail(email) {
+  return supabaseClient.from('enroll').select('*').eq('email', email)
+}
+
 export function insertEnrollment(enrollment) {
   return supabaseClient.from('enroll').insert(enrollment)
 }
