2806b686c3c202fc5cdfb18a87aebf0a23e5acd1
Alireza Balouch <gswape@gmail.com>
2024-09-19 19:16:38 +0200

    fetch data from passage and backend

---
+++ src/utils/dataFetching.js
@@ -0,0 +1,44 @@
+import {Passage} from '@passageidentity/passage-js'
+import {LOGIN_API, API_URL} from '../config.js'
+
+import {authStateReady, user, auth} from '../store.js'
+
+const passage = new Passage(LOGIN_API)
+
+export async function fetchUserAuth() {
+  const userP = passage.currentUser
+  return await userP
+    .userInfo()
+    .then((userRes) => {
+      if (userRes) {
+        auth.set(true)
+        user.set(userRes)
+
+        const additionalInfo = getUserInfo(userRes.id)
+        // TODO: filter out userRes and store to backend
+        return {...additionalInfo, ...userRes}
+      }
+    })
+    .catch((err) => {
+      console.log('wow some error happened', err)
+    })
+    .finally(() => {
+      authStateReady.set(true)
+    })
+}
+export async function fetchApi(path, method = 'GET', body = null) {
+  const passageToken = localStorage.getItem('psg_auth_token') ?? null
+
+  return await fetch(`${API_URL}${path}`, {method, body, headers: {Authorization: `Bearer ${passageToken}`}})
+    .then((res) => {
+      if (res.status === 401) {
+        // TODO send to login page
+        return null
+      }
+
+      return res.json()
+    })
+    .catch((e) => {
+      console.log('e ...', e)
+    })
+}
