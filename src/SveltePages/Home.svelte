8c9a664d656a5880a9a157596d82cd3cdab07b47
Alireza Balouch <gswape@gmail.com>
2024-11-13 14:00:48 +0100

    using isAdmin to show debug info

--- src/SveltePages/Home.svelte
+++ src/SveltePages/Home.svelte
@@ -1,5 +1,5 @@
 <script>
-import {user, myRoles, currentRole, schoolNames} from '../store.js'
+import {user, myRoles, currentRole, isAdmin} from '../store.js'
 import Card from '../components/Card.svelte'
 import {roleTitles} from '../constants.ts'
 
@@ -9,7 +9,7 @@ function resetRole() {
 </script>
 
 {#if $user?.uid}
-  <h1 class="text-xl text-center p-3">{`Hei ${$user.name || ''}`}</h1>
+  <h1 class="text-2xl text-center p-3">{`Hei ${$user.name || ''}`}</h1>
 
   <section class="grid grid-cols-1 md:grid-cols-2 gap-3 m-4 xl:grid-cols-3">
     {#if $myRoles?.length > 1}
@@ -22,32 +22,33 @@ function resetRole() {
         </div>
       </Card>
     {/if}
-
-    <Card title="debugging">
-      <dl class="flex gap-3">
-        <dt class="w-20 font-bold">UID</dt>
-        <dd>{$user.uid}</dd>
-      </dl>
-      <dl class="flex gap-3">
-        <dt class="w-20 font-bold">Name</dt>
-        <dd>{$user.name || ''}</dd>
-      </dl>
-      <dl class="flex gap-3">
-        <dt class="w-20 font-bold">PHONE</dt>
-        <dd>{$user.phone || ''}</dd>
-      </dl>
-      <dl class="flex gap-3">
-        <dt class="w-20 font-bold">EMAIL</dt>
-        <dd>{$user.email}</dd>
-      </dl>
-      <dl class="flex gap-3">
-        <dt class="w-20 font-bold">Sid</dt>
-        <dd>{$currentRole?.sid}</dd>
-      </dl>
-      <dl class="flex gap-3">
-        <dt class="w-20 font-bold">Level</dt>
-        <dd>{$currentRole?.level}</dd>
-      </dl>
-    </Card>
+    {#if $isAdmin}
+      <Card title="debugging">
+        <dl class="flex gap-3">
+          <dt class="w-20 font-bold">UID</dt>
+          <dd>{$user.uid}</dd>
+        </dl>
+        <dl class="flex gap-3">
+          <dt class="w-20 font-bold">Name</dt>
+          <dd>{$user.name || ''}</dd>
+        </dl>
+        <dl class="flex gap-3">
+          <dt class="w-20 font-bold">PHONE</dt>
+          <dd>{$user.phone || ''}</dd>
+        </dl>
+        <dl class="flex gap-3">
+          <dt class="w-20 font-bold">EMAIL</dt>
+          <dd>{$user.email}</dd>
+        </dl>
+        <dl class="flex gap-3">
+          <dt class="w-20 font-bold">Sid</dt>
+          <dd>{$currentRole?.sid}</dd>
+        </dl>
+        <dl class="flex gap-3">
+          <dt class="w-20 font-bold">Level</dt>
+          <dd>{$currentRole?.level}</dd>
+        </dl>
+      </Card>
+    {/if}
   </section>
 {/if}
