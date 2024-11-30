a8eae2f3b5867a8720c05344fceca3e523131019
Alireza Balouch <gswape@gmail.com>
2024-11-30 15:49:40 +0100

    improving empty placeholder

--- src/components/EmptyPlaceholder.svelte
+++ src/components/EmptyPlaceholder.svelte
@@ -2,7 +2,7 @@
 export let message = 'Ingen data'
 </script>
 
-<div class="p-4 flex flex-col gap-4 items-center">
+<div class="p-4 flex flex-col gap-4 items-center mt-8">
   <img src="/svg/undraw_void_-3-ggu.svg" alt="Illustrasjon av et tomt rom" class="max-w-sm w-full" />
   <p class="pt-4">{message}</p>
 </div>
