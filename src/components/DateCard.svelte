d9ea312453b7e22292026f964d1a7f4d33ced9ad
Alireza Balouch <gswape@gmail.com>
2024-12-09 17:05:21 +0100

    improve dateCard

--- src/components/DateCard.svelte
+++ src/components/DateCard.svelte
@@ -1,12 +1,26 @@
 <script>
 import {getDay, getLocaleDayName, getLocaleMonthName, getYear} from '../utils/date'
 export let date
+
+function isOldDueDate(date) {
+  return new Date(date) < new Date()
+}
+
+function oldColor(date, isBorder = false) {
+  if (isBorder) {
+    return isOldDueDate(date) ? 'border-gray-400 opacity-80' : 'border-primary'
+  }
+  return isOldDueDate(date) ? 'bg-gray-700' : 'bg-primary'
+}
 </script>
 
 <div
-  class="border border-primary rounded-md flex-col text-center inline-flex w-24 overflow-hidden bg-base-100 text-base-content">
-  <div class="text-sm bg-primary text-primary-content px-3">{getLocaleMonthName(date)}</div>
+  class="{oldColor(
+    date,
+    true
+  )} border rounded-md flex-col text-center inline-flex w-24 overflow-hidden bg-base-100 text-base-content">
+  <div class="text-sm {oldColor(date)}  text-primary-content px-3">{getLocaleMonthName(date)}</div>
   <div class="text-sm pt-3">{getLocaleDayName(date)}</div>
   <div class="text-3xl px-3 pb-3">{getDay(date)}</div>
-  <div class="text-sm bg-primary text-primary-content px-3">{getYear(date)}</div>
+  <div class="text-sm {oldColor(date)}  text-primary-content px-3">{getYear(date)}</div>
 </div>
