36d2040bedc7beda959d21494c72a2631cbe78b8
Alireza Balouch <gswape@gmail.com>
2025-01-20 10:57:05 +0100

    clean up

--- src/apiCalls/user.js
+++ src/apiCalls/user.js
@@ -1,8 +1,6 @@
 import {supabase} from '../supabase.js'
 import {allUsers} from '../store.js'
 
-export function getUserInfo() {}
-
 export function getRegisteredUsers(sid) {
   supabase
     .from('users_roles')
@@ -14,15 +12,6 @@ export function getRegisteredUsers(sid) {
       }
     })
 }
-
-export async function getLevelFromRoleAndUser(rid, uid) {
-  const res = await supabase.from('users_roles').select('level').eq('uid', uid).eq('rid', rid)
-  if (res?.data?.length > 0) {
-    return res.data[0].level
-  }
-}
-
-export function deleteUser(sid, uid) {}
 
 export async function saveEditedUser(data) {
   await supabase.from('user_info').update(data).eq('uid', data.uid).select()
