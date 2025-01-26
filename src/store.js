4de53fabfb749e706b3f210d0460c1f7290e0e21
Alireza Balouch <gswape@gmail.com>
2025-01-26 17:12:26 +0100

    rearrange stores

--- src/store.js
+++ src/store.js
@@ -1,27 +1,32 @@
 import {writable} from 'svelte/store'
 
+// common stores
 export const authStateReady = writable(false)
 export const auth = writable(false)
-export const currentPage = writable('home')
-export const user = writable(null)
-export const myRoles = writable(null)
-export const currentRole = writable(null)
 export const schoolNames = writable(null)
-export const rooms = writable(null)
-export const usersRooms = writable(null)
-
-export const groups = writable(null)
-export const groupUserCount = writable(null)
-
-export const menuList = writable(null)
-export const currentRoom = writable(null)
-
-export const allUsers = writable(null)
-export const unregisteredUsers = writable(null)
-
 export const isAdmin = writable(false)
 export const isTeacherOrAdmin = writable(false)
 export const sid = writable(null)
+export const currentPage = writable('home')
+export const menuList = writable(null)
+
+// user stores
+export const user = writable(null)
+export const myRoles = writable(null)
+export const currentRole = writable(null)
+
+// room stores
+export const rooms = writable(null)
+export const usersRooms = writable(null)
+export const currentRoom = writable(null)
+
+// teacher or admin stores
+export const groups = writable(null)
+export const groupUserCount = writable(null)
+
+// admin stores
+export const allUsers = writable(null)
+export const unregisteredUsers = writable(null)
 
 currentRole.subscribe((value) => {
   if (value) {
