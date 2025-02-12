fa3291533de4da73890ed6a12acb43009a10966c
Alireza Balouch <gswape@gmail.com>
2025-02-12 11:42:08 +0100

    refactor: clean up HTML structure and streamline JavaScript export

--- src/lms.js
+++ src/lms.js
@@ -2,8 +2,6 @@ import {mount} from 'svelte'
 import './app.css'
 import App from './app.svelte'
 
-const lms = mount(App, {
+export default mount(App, {
   target: document.getElementById('app')
 })
-
-export default lms
