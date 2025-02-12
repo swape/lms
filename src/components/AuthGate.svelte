f7cbc36953a74ddef8a78ed857453b2ff3b02e8f
Alireza Balouch <gswape@gmail.com>
2025-02-12 09:14:14 +0100

    populate rooms and groups on start

--- src/components/AuthGate.svelte
+++ src/components/AuthGate.svelte
@@ -1,6 +1,6 @@
 <script>
 import Login from './Login.svelte'
-import {auth, currentRole, authStateReady} from '../store.js'
+import {auth, currentRole, authStateReady, sid} from '../store.js'
 import RoleSelect from './RoleSelect.svelte'
 import LoadingSpinner from './LoadingSpinner.svelte'
 import {getRoles} from '../apiCalls/roles.js'
@@ -8,6 +8,7 @@ import {getSchools} from '../apiCalls/schools.js'
 import {user} from '../store.js'
 import {saveStorage} from '../utils/localStorage.ts'
 import {onMount} from 'svelte'
+import {populateRoomsAndGroups} from '../services.js'
 
 const {children} = $props()
 
@@ -23,6 +24,7 @@ user.subscribe(async (value) => {
 
     await getSchools()
     await getRoles(uid)
+    await populateRoomsAndGroups($sid, $currentRole, true)
   }
 })
 </script>
