41b407afe7349b7c44463c7fe33e9546a90658a2
Alireza Balouch <gswape@gmail.com>
2025-02-14 12:47:46 +0100

    feat: add support for rendering children in RoomMessageCard component

--- src/pages/Rooms/RoomMessageCard.svelte
+++ src/pages/Rooms/RoomMessageCard.svelte
@@ -5,7 +5,7 @@ import {isTeacherOrAdmin} from '../../store.js'
 import {isOldDueDate} from '../../utils/date.ts'
 import Button from '../../components/Button.svelte'
 
-const {localMessage = {}, openThisMessage} = $props()
+const {localMessage = {}, openThisMessage = () => {}, children} = $props()
 </script>
 
 <Card title={localMessage.title}>
@@ -13,10 +13,16 @@ const {localMessage = {}, openThisMessage} = $props()
     <div class={isOldDueDate(localMessage.dueDate || new Date()) ? 'text-gray-400' : ''}>
       {localMessage.message}
     </div>
+
     {#if localMessage.dueDate}
       <DateCard date={localMessage.dueDate} />
     {/if}
   </div>
+  {#if children}
+    <div>
+      {@render children()}
+    </div>
+  {/if}
   {#if $isTeacherOrAdmin}
     <div class="card-actions">
       <Button action={() => openThisMessage(localMessage)} text="Rediger" />
