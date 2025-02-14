694ad351ad19d50690d331d8e6d6fbb22c9505be
Alireza Balouch <gswape@gmail.com>
2025-02-14 12:46:47 +0100

    feat: add function to retrieve room messages from an array of room IDs with caching

--- src/apiCalls/roomMessages.js
+++ src/apiCalls/roomMessages.js
@@ -28,3 +28,25 @@ export async function getRoomMessages(roomId, force = false) {
 export async function upsertRoomMessage(message) {
   return supabase.from('room_messages').upsert(message).select()
 }
+
+export async function getRoomMessagesFromRoomArray(list, force = false) {
+  const roomMessageName = `room-messages-from-array-${list.join('-')}`
+  const roomMessages = getStorage(roomMessageName, 2)
+
+  if (roomMessages?.length && !force) {
+    return roomMessages
+  }
+
+  return await supabase
+    .from('room_messages')
+    .select('*')
+    .in('roomId', list)
+    .then((res) => {
+      // @ts-ignore
+      if (res.data?.length > 0) {
+        saveStorage(roomMessageName, res.data)
+        return res.data
+      }
+      return []
+    })
+}
