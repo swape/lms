32189871c8c45250426334961621f9215f07e158
Alireza Balouch <gswape@gmail.com>
2025-02-14 12:46:24 +0100

    style: enhance Card component layout with flexbox for better child alignment

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -2,7 +2,7 @@
 const {title = '', children} = $props()
 </script>
 
-<div class="card border border-base-300 shadow-sm m-2 p-5 bg-base-200 gap-3">
+<div class="card border border-base-300 shadow-sm m-2 p-5 bg-base-200 gap-3 flex flex-col justify-between">
   {#if title}<h2 class="card-title">{title}</h2>{/if}
   {@render children()}
 </div>
