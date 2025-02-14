86b13f0405277021b9eccdacf9577d1e1618810c
Alireza Balouch <gswape@gmail.com>
2025-02-14 12:48:03 +0100

    feat: integrate room messages display in Home component with dynamic loading

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,12 +1,40 @@
 <script>
-import {user, myRoles, currentRole, isTeacherOrAdmin} from '../store.js'
+import {user, myRoles, currentRole, isTeacherOrAdmin, usersRooms, currentPage, currentRoom} from '../store.js'
 import Card from '../components/Card.svelte'
 import RoleName from '../components/RoleName.svelte'
 import {getSchoolName} from '../utils/helper.ts'
 import Button from '../components/Button.svelte'
+import {getRoomMessagesFromRoomArray} from '../apiCalls/roomMessages.js'
+import {isOldDueDate} from '../utils/date.ts'
+import RoomMessageCard from './Rooms/RoomMessageCard.svelte'
+
+let messages = $state([])
+let localRooms = $state([])
 
 function resetRole() {
   $currentRole = null
+}
+
+usersRooms.subscribe((value) => {
+  if (value) {
+    localRooms = value
+    const roomsList = value.map((room) => room.id)
+    getRoomMessagesFromRoomArray(roomsList).then((data) => {
+      messages = data.filter((message) => !isOldDueDate(message.dueDate || new Date()))
+    })
+  }
+})
+
+function getRoomName(roomId) {
+  const room = localRooms.find((room) => room.id === roomId)
+  return room?.title || ''
+}
+
+function gotoRoom(roomId) {
+  const room = localRooms.find((room) => room.id === roomId)
+
+  currentRoom.set(room)
+  $currentPage = 'rooms'
 }
 </script>
 
@@ -24,6 +52,16 @@ function resetRole() {
           <Button action={resetRole} icon="supervisor_account" text="Bytt rolle" classList="btn-primary" />
         </div>
       </Card>
+    {/if}
+
+    {#if $currentRole.level === 1 || $currentRole.level === 2}
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 mt-4">
+        {#each messages as message}
+          <RoomMessageCard localMessage={message}>
+            <Button text={getRoomName(message.roomId)} action={() => gotoRoom(message.roomId)} />
+          </RoomMessageCard>
+        {/each}
+      </div>
     {/if}
   </section>
 {/if}
